fe04868af53240b312d9fc0b9977a5e076254dd4
zkan <zkan.cs@gmail.com>
2020-08-04 21:12:26 +0700

    Check type of execution date passed via context :bear:

--- playground/dags/execution_date.py
+++ playground/dags/execution_date.py
@@ -1,6 +1,14 @@
+
+
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
+
+
+def split_execution_date(**kwargs):
+    execution_date = kwargs['execution_date']
+    print(execution_date, type(execution_date))
 
 
 args = {
@@ -14,4 +22,10 @@ with DAG('execution_date', schedule_interval='*/5 * * * *', default_args=args, c
         bash_command='echo {{ ds }} {{ execution_date }} {{ ts }}',
     )
 
-    t0
+    t1 = PythonOperator(
+        task_id='split_execution_date',
+        python_callable=split_execution_date,
+        op_kwargs={'execution_date': '{{ ds }}'},
+    )
+
+    t0 >> t1
