fd328dd5550070d6c7b45b67cd7a7b9740aea473
zkan <zkan.cs@gmail.com>
2021-08-13 18:49:00 +0700

    Revise DAG style and use new import style :bear:

--- mnt/airflow/dags/test_scheduling_notebooks.py
+++ mnt/airflow/dags/test_scheduling_notebooks.py
@@ -1,34 +1,31 @@
 from airflow import DAG
-from airflow.operators.papermill_operator import PapermillOperator
+from airflow.providers.papermill.operators.papermill import PapermillOperator
 from airflow.utils import timezone
 
+
+NOTEBOOKS_FOLDER = '/usr/local/airflow/dags/notebooks'
 
 default_args = {
     'owner': 'zkan'
 }
-dag = DAG(
-    'test_scheduling_notebooks',
-    schedule_interval='*/5 * * * *',
-    default_args=default_args,
-    start_date=timezone.datetime(2020, 8, 15),
-    catchup=False
-)
+with DAG('test_scheduling_notebooks',
+         schedule_interval='*/5 * * * *',
+         default_args=default_args,
+         start_date=timezone.datetime(2020, 8, 15),
+         catchup=False) as dag:
 
-NOTEBOOKS_FOLDER = '/usr/local/airflow/dags/notebooks'
+    t1 = PapermillOperator(
+        task_id='t1',
+        input_nb=f'{NOTEBOOKS_FOLDER}/input.ipynb',
+        output_nb=f'{NOTEBOOKS_FOLDER}/output.ipynb',
+        parameters=dict(name='ODDS', x=0.1, y=10),
+    )
 
-t1 = PapermillOperator(
-    task_id='t1',
-    input_nb=f'{NOTEBOOKS_FOLDER}/input.ipynb',
-    output_nb=f'{NOTEBOOKS_FOLDER}/output.ipynb',
-    parameters=dict(name='ODDS', x=0.1, y=10),
-    dag=dag,
-)
-t2 = PapermillOperator(
-    task_id='t2',
-    input_nb=f'{NOTEBOOKS_FOLDER}/input.ipynb',
-    output_nb=f'{NOTEBOOKS_FOLDER}/output-{{{{ execution_date }}}}.ipynb',
-    parameters={'msg': 'Ran from Airflow at {{ execution_date }}!'},
-    dag=dag,
-)
+    t2 = PapermillOperator(
+        task_id='t2',
+        input_nb=f'{NOTEBOOKS_FOLDER}/input.ipynb',
+        output_nb=f'{NOTEBOOKS_FOLDER}/output-{{{{ execution_date }}}}.ipynb',
+        parameters={'msg': 'Ran from Airflow at {{ execution_date }}!'},
+    )
 
-t1 >> t2
+    t1 >> t2
